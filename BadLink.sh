c4d3bec9a685607b7b543a0e0096a311a9de24b0
CODEreaper177 <114791478+CODEreaper177@users.noreply.github.com>
2022-12-17 22:53:37 -0500

    BadLink.sh

---
+++ BadLink.sh
@@ -0,0 +1,37 @@
+echo -ne '####                     (10%)\r'
+sleep 1
+echo -ne '###########             (43%)\r'
+sleep 1
+echo -ne '#################  (65%)\r'
+echo -ne '\n'
+echo -ne '#####################  (70%)\r'
+sleep 1
+echo -ne '###########################  (98%)\r'
+sleep 1
+echo -ne '##############################  (100%)\r'
+echo -ne '\n'
+Echo 
+Echo
+Echo [Hybrid] Script, successfully Activated!!!
+Echo
+Echo
+Echo
+echo -ne '####                     (loadingLink...)\r'
+sleep 1
+echo -ne '###########             (loadingLink...)\r'
+sleep 1
+echo -ne '#################  (loadingLink...)\r'
+echo -ne '\n'
+echo -ne '#####################  (loadingLink...)\r'
+sleep 1
+echo -ne '###########################  (loadingLink...)\r'
+sleep 1
+echo -ne '##############################  (loadingLink...)\r'
+echo -ne '\n'
+Echo
+Echo
+Echo [Hybrid] BadLink, Here.....
+Echo
+Echo
+Echo       
+
